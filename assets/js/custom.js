f1979e224e7537d724dd7835fb01e547660951fe
Galanta <neil.angelo@hotmail.com>
2022-08-18 13:19:19 -0300

    updating behavior

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,33 @@
-function register(){
-    
-    description = document.getElementById("description").ariaValueMax;
+function register() {
 
+    mode = document.getElementById("mode").value;
+    description = document.getElementById("description").value;
+    amount = document.getElementById("amount").value;
+
+    if (mode || amount || description != "") {
+        console.log('Muito bem!')
+    } else {
+        console.log("Você precisa preencher os campos")
+        Toasty();
+
+
+
+    }
+    console.log(mode)
     console.log(description)
+    console.log(amount)
 }
-    
+var option =
+{
+    animation: true,
+    delay: 1000
+};
+
+function Toasty() {
+    var toastHTMLElement = document.getElementById('alert');
+
+    var toastElement = new bootstrap.Toast(toastHTMLElement, option);
+
+    toastElement.show();
+}
+
